14b3800425311548c2dbf594b53c69ff055ae47f
annuraagggIIIT <anuragkhanna@iiitmanipur.ac.in>
2024-10-06 11:55:45 +0530

    initial

--- src/lib.rs
+++ src/lib.rs
@@ -7,12 +7,12 @@ use solana_program::{
 };
 entrypoint!(my_defi_app);
 fn my_defi_app(
-    program_id: &[Pubkey],
+    program_id: &Pubkey,
     account:&[AccountInfo],
     instruction_data: &[u8],
 
 ) -> ProgramResult {
     msg!("hello , welcome to our Dapp");
     msg!("token buy karlo pls");
-    ok(())
+    Ok(())
 }
